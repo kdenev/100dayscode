c6562148bfde5d98e3d5fc276494e9ef4f671f0e
kdenev <kristiyan.denev@gmail.com>
2023-12-16 22:35:01 +0100

    Day 21 main

---
+++ day21/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+my_screen = Screen()
+my_screen.setup(width=600, height=600)
+my_screen.bgcolor("black")
+my_screen.title("Snake Game")
+my_screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.left, "Left")
+my_screen.onkey(snake.right, "Right")
+
+my_screen.update()
+
+game_on = True
+while game_on:
+
+    my_screen.update()
+    time.sleep(.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.grow()
+        scoreboard.add()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for t in snake.snake[1:]:
+        if snake.head.distance(t) < 10:
+            game_on = False
+            scoreboard.game_over()
+
+my_screen.exitonclick()
