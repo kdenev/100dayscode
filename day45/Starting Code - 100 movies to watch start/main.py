92b4c4f016a6c97b7d16e2b52601069cb5e93f46
kdenev <kristiyan.denev@gmail.com>
2024-01-29 20:47:41 +0100

    Day 46 Complete

--- day45/Starting Code - 100 movies to watch start/main.py
+++ day45/Starting Code - 100 movies to watch start/main.py
@@ -5,4 +5,9 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(url=URL)
+soup = BeautifulSoup(response.text, "html.parser")
+titles = [title.getText() for title in soup.find_all(name="h3", class_="title")]
 
+file =  open(r'day45\Starting Code - 100 movies to watch start\titles.txt', 'w', encoding='utf-8')
+file.write("\n".join(titles[::-1]))
--- day45/exercise.py
+++ day45/exercise.py
@@ -8,7 +8,10 @@ yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
 
-article_contents = soup.find(name="span", class_="titleline")
-article_text = article_contents.getText()
-article_link = article_contents.find("a").get("href")
-article_upvote = soup.find(name="span", class_="score").getText()
+article_contents = soup.find_all(name="span", class_="titleline")
+article_text = [article.getText() for article in article_contents]
+article_link = [article.find("a").get("href") for article in article_contents]
+article_upvote = [int(span.getText().split()[0]) for span in soup.find_all(name="span", class_="score")]
+
+most_upvoted_index = article_upvote.index(max(article_upvote))
+print(article_text[most_upvoted_index], article_link[most_upvoted_index])
