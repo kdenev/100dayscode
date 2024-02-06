6d201fa6b370d03d7c78dceee9d391ee04baf216
kdenev <kristiyan.denev@gmail.com>
2024-02-06 19:27:19 +0100

    Day 50 Skipped

---
+++ day50/start.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+import sys
+import os
+# Import api script
+sys.path.append(r"D:\Desktop\code\python\api_key_creator")
+import api_key_creator
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+URL = "https://tinder.com/app/recs"
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url=URL)
+
